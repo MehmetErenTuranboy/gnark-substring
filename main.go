4bf23f0b66f05021723706fbf8e5d57c640a5193
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-10-23 15:27:49 +0200

    add assertisequal control

--- main.go
+++ main.go
@@ -17,27 +17,14 @@ type charEqualityCircuit struct {
 }
 
 func (circuit *charEqualityCircuit) Define(api frontend.API) error {
-	// Calculate (A - B)^2
+	// Calculate (A - B)
 	diff := api.Sub(circuit.A, circuit.B)
-	squaredDiff := api.Mul(diff, diff)
 
-	// If A and B are equal, (A - B)^2 = 0, otherwise it is a positive number.
-	// We want AreEqual to be 1 if A and B are equal, and 0 otherwise.
+	// Create a binary variable that is 1 if diff is 0, and 0 otherwise
+	isEqual := api.IsZero(diff)
 
-	// To create a constant value in the circuit, you can directly assign it.
-	constantOne := 1
-
-	// In gnark, to check if a value is non-zero, you can use api.DivUnchecked(1, squaredDiff).
-	// This will be equal to 0 if squaredDiff is non-zero (because 1/non-zero is a small number, close to 0),
-	// and will be equal to a very large number if squaredDiff is 0 (because 1/0 is infinity in field arithmetic).
-	// However, this approach may have issues if squaredDiff is indeed zero. So be careful with this approach.
-	inverseSquaredDiff := api.DivUnchecked(constantOne, squaredDiff)
-
-	// Now, subtract the result from 1 to get AreEqual
-	areEqual := api.Sub(constantOne, inverseSquaredDiff)
-
-	// Assert that the computed areEqual value matches the public AreEqual variable
-	api.AssertIsEqual(circuit.AreEqual, areEqual)
+	// Assert that the binary variable matches the public AreEqual variable
+	api.AssertIsEqual(circuit.AreEqual, isEqual)
 
 	return nil
 }
@@ -47,7 +34,7 @@ func main() {
 
 	// Secret values (characters 'A' and 'B' for example)
 	a := big.NewInt(int64('A'))
-	b := big.NewInt(int64('B'))
+	b := big.NewInt(int64('C'))
 
 	// Expected result: 0 (since 'A' is not equal to 'B')
 	expectedResult := big.NewInt(0)
