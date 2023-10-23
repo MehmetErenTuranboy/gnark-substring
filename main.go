1385a72b2561886e2ad879e154b24eedc82441bc
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-10-23 16:15:50 +0200

    feat: add comparing with array implementation

--- main.go
+++ main.go
@@ -12,19 +12,34 @@ import (
 )
 
 type charEqualityCircuit struct {
-	A, B     frontend.Variable `gnark:",secret"`
-	AreEqual frontend.Variable `gnark:",public"` // AreEqual is public, 1 if A and B are equal, 0 otherwise
+	A        [3]frontend.Variable `gnark:",secret"`
+	B        frontend.Variable   `gnark:",secret"`
+	AreEqual frontend.Variable   `gnark:",public"` // AreEqual is public, 1 if any element in A is equal to B, 0 otherwise
 }
 
-func (circuit *charEqualityCircuit) Define(api frontend.API) error {
-	// Calculate (A - B)
-	diff := api.Sub(circuit.A, circuit.B)
+// ... (previous code)
 
-	// Create a binary variable that is 1 if diff is 0, and 0 otherwise
-	isEqual := api.IsZero(diff)
+func (circuit *charEqualityCircuit) Define(api frontend.API) error {
+	// Initialize a variable to store if any comparison was successful
+	anyEqual := frontend.Variable(0)
+
+	for _, a := range circuit.A {
+		// Calculate (a - B)
+		diff := api.Sub(a, circuit.B)
+
+		// Create a binary variable that is 1 if diff is 0, and 0 otherwise
+		isEqual := api.IsZero(diff)
+
+		// If any of the comparisons is true, set anyEqual to 1
+		anyEqual = api.Add(anyEqual, isEqual)
+	}
+
+	// Convert anyEqual to a binary variable (0 or 1)
+	// Here you might need to implement the logic for IsNonZero if it's not available
+	areEqual := anyEqual // Replace this with the correct logic
 
 	// Assert that the binary variable matches the public AreEqual variable
-	api.AssertIsEqual(circuit.AreEqual, isEqual)
+	api.AssertIsEqual(circuit.AreEqual, areEqual)
 
 	return nil
 }
@@ -32,11 +47,15 @@ func (circuit *charEqualityCircuit) Define(api frontend.API) error {
 func main() {
 	var circuit charEqualityCircuit
 
-	// Secret values (characters 'A' and 'B' for example)
-	a := big.NewInt(int64('A'))
-	b := big.NewInt(int64('A'))
+	// Secret values
+	a := [3]*big.Int{
+		big.NewInt(int64('A')),
+		big.NewInt(int64('B')),
+		big.NewInt(int64('C')),
+	}
+	b := big.NewInt(int64('C'))
 
-	// Expected result: 0 (since 'A' is not equal to 'B')
+	// Expected result: 1 (since 'A' is in the array)
 	expectedResult := big.NewInt(1)
 
 	// Compile the circuit into a set of constraints
@@ -52,10 +71,15 @@ func main() {
 	}
 
 	assignment := charEqualityCircuit{
-		A:        *a,
-		B:        *b,
-		AreEqual: *expectedResult,
+		A: [3]frontend.Variable{
+			frontend.Variable(a[0]),
+			frontend.Variable(a[1]),
+			frontend.Variable(a[2]),
+		},
+		B:        frontend.Variable(b),
+		AreEqual: frontend.Variable(expectedResult),
 	}
+	
 
 	// Create a witness from the assignment
 	witness, err := frontend.NewWitness(&assignment, ecc.BN254)
